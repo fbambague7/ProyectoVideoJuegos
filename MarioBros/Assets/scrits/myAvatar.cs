cce396b5ff20ade6cacaacfcddb485a3007dbd4e
millercastillo <37094202+millercastillo@users.noreply.github.com>
2019-03-13 23:07:46 -0500

    movimiento Mejorado mario
    se mejora los movimientos de mario y alguno colisionadores de la cerca

--- MarioBros/Assets/scrits/myAvatar.cs
+++ MarioBros/Assets/scrits/myAvatar.cs
@@ -4,30 +4,47 @@ using UnityEngine;
 
 public class myAvatar : MonoBehaviour {
 
-    private Transform myTrasform;
+    private Vector3 moveVelocity;
     public float velocidad;
     private float xAxis, zAxis;
+    private Camera mainCamera;
+    private Rigidbody rb;
 
     // Use this for initialization
     void Start()
     {
-        myTrasform = GetComponent<Transform>();
-
+        mainCamera = Camera.main;
+        rb = GetComponent<Rigidbody>();
     }
-
     // Update is called once per frame
     void FixedUpdate()
     {
+        //rb.velocity = moveVelocity;
+        rb.MovePosition(transform.position + moveVelocity);
+    }
+    private void Update()
+    {
         xAxis = Input.GetAxis("Horizontal");
         zAxis = Input.GetAxis("Vertical");
-        Vector3 movement = new Vector3(-zAxis, 0, xAxis);
-        myTrasform.Translate(movement * velocidad);
-
-        if (Input.GetKey(KeyCode.X))
+        Vector3 movement = new Vector3(xAxis, 0, zAxis);
+        Vector3 cameraForward = mainCamera.transform.forward;
+        cameraForward.y = 0;
+        Quaternion cameraRelativeRotacion = Quaternion.FromToRotation(Vector3.forward, cameraForward);
+        Vector3 lookToward = cameraRelativeRotacion * movement;
+        if (movement.sqrMagnitude > 0)
         {
-            transform.Translate(0, Time.deltaTime * 6, 0);
-
+            Ray lookRay = new Ray(transform.position, lookToward);
+            transform.LookAt(lookRay.GetPoint(1));
         }
-       
+        moveVelocity = transform.forward * velocidad * Time.deltaTime * movement.sqrMagnitude;
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            
+            rb.AddForce(Vector3.up * 200);
+        }
+
     }
+
+   
 }
