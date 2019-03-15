e16a11d8e1eddebe6996dbcc5902956d643743a8
millercastillo <37094202+millercastillo@users.noreply.github.com>
2019-03-14 20:36:21 -0500

    MenuMario
    Se adiciona la esena del menu y el contador del tiempo

---
+++ MarioBros/Assets/scrits/TimerContador.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerContador : MonoBehaviour
+{
+    public Text contador;
+    public Text fin;
+    private float Tiempo = 60f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        contador.text = "" + Tiempo;
+        fin.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Tiempo -= Time.deltaTime;
+        contador.text = "" + Tiempo.ToString("f0");
+        if (Tiempo <= 0)
+        {
+            contador.text = "0";
+            fin.enabled = true;
+        }
+    }
+}
