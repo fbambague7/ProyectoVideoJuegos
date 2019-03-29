e19ceee987ed5c83146a530ee380c89fc4e75bf3
fbambague7 <36634463+fbambague7@users.noreply.github.com>
2019-03-28 23:43:51 -0500

    SaltoCubos
    saltan los cubos cuando colisionan

--- MarioBros/Assets/scrits/Script_Moneda.cs
+++ MarioBros/Assets/scrits/Script_Moneda.cs
@@ -11,35 +11,72 @@ public class Script_Moneda : MonoBehaviour {
     public float fuerzaSalto;
     private int numMonedas;
 
+    
+    bool colisiona = false;
+    Animator animator;
+
+    void start()
+    {
+        
+    }
 
 
     // Update is called once per frame
 
     void Update() {
-
+        
     }
 
     private void OnCollisionEnter(Collision col)
     {
         if (col.gameObject.CompareTag("mario"))
         {
+            saltarCubos();
             saltarMonedas();
             numMonedas = numMonedas + 1;
 
+            
         }
+
         if (numMonedas >= 5) {
             Destroy(gameObject, .10f);
            // 
         }
-
     }
     void saltarMonedas() {
         monedaClon = Instantiate(moneda, cubo.GetComponent<Transform>().position, Quaternion.identity);
         monedaClon.GetComponent<Rigidbody>().AddForce(new Vector3(0,fuerzaSalto,0));
         Destroy(monedaClon,.50f);
-
-
-
         
+
     }
+
+    void saltarCubos()
+    {
+        animator = cubo.gameObject.GetComponent<Animator>();
+        this.animator.SetBool("colisiona", true);
+        Debug.Log("valor Enter: " + this.animator.GetBool("colisiona"));
+        StartCoroutine(Example());
+    }
+
+    
+    private void onCollisonExit()
+    {
+        animator = cubo.gameObject.GetComponent<Animator>();
+        this.animator.SetBool("colisiona", false);
+        Debug.Log("valor Exit: " + this.animator.GetBool("colisiona"));
+    }
+
+    IEnumerator Example()
+    {
+        
+        Debug.Log(Time.time);
+        yield return new WaitForSeconds(0.15f);
+        Debug.Log(Time.time);
+
+        animator = cubo.gameObject.GetComponent<Animator>();
+        this.animator.SetBool("colisiona", false);
+        Debug.Log("valor Exit: " + this.animator.GetBool("colisiona"));
+    }
+
 }
